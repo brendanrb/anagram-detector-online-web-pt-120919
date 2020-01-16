7d191a6b7d5c97c26fc59711d7ad501f8e29c290
Brendan R Borgers <brendanborgers@gmail.com>
2020-01-16 00:36:55 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,8 +8,9 @@ class Anagram
   end
   
   def match(array)
-   if word.split("").sort == array.sort
-     
-  
+   array_of_words.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
+    end
+  end
   
 end
